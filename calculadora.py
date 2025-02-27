7e6499b892d52e58c915b21e365c34e3b17c5d86
deivyg4 <deivyg374@gmail.com>
2025-02-27 14:20:36 -0400

    calculadora de suma

---
+++ calculadora.py
@@ -0,0 +1,10 @@
+a = int(input("ingrese el primer numero :>__"))
+
+b = int(input("ingrese el segundo numero :>__"))
+
+def suma(a, b):
+    resultado = a + b
+    print("El resultado es:", resultado)
+    return resultado
+      
+suma(a, b)
