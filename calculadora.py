1476c134df5180997cbca22224b57050f3c308fd
deivyg4 <deivyg374@gmail.com>
2025-02-27 14:37:24 -0400

    prueba de reset

--- calculadora.py
+++ calculadora.py
@@ -1,18 +1,1 @@
-a = int(input("ingrese el primer numero :>__"))
-
-b = int(input("ingrese el segundo numero :>__"))
-
-def suma(a, b):
-    resultado = a + b
-    print("El resultado es:", resultado)
-    return resultado
-      
-suma(a, b)
-
-a=int(imput("Ingrese el primer numero:>_ "))
-b=int(imput("Ingrese el segundo numero:>_"))
-
-def resta (a,b):
-    resultado=a-b
-    print(resultado)
-    return resultado
+print("pene")
