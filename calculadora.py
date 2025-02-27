28baa4e6b88c741d69f2c6d82d71d4f26536bbdf
Miguel-dh <bungouseven@gmail.com>
2025-02-27 14:17:23 -0400

    commit1: funcion de multiplicacion

---
+++ calculadora.py
@@ -0,0 +1,9 @@
+a = input("Ingrese el primer numero:>_")
+b = input("Ingrese el segundo numero:>_")
+
+def multiplicacion():
+    resultado = a * b
+    print("El resultado es:", resultado)
+    return resultado
+
+multiplicacion()
